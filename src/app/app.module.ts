cafc7aff981c61e2fbe1089632b22f31aaafaece
PhanThanhDong-Crush <97138310+PhanThanhDong-Crush@users.noreply.github.com>
2023-05-16 23:58:01 +0700

    file product admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/Client/product-list/product-list.component';
 import { ProductDetailComponent } from './components/Client/product-detail/product-detail.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/Client/home/home.component';
+import { ProductsComponent } from './components/Admin/products/products.component';
+import { ProductAddComponent } from './components/Admin/product-add/product-add.component';
+import { ProductEditComponent } from './components/Admin/product-edit/product-edit.component';
 
 @NgModule( {
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductDetailComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    ProductAddComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
