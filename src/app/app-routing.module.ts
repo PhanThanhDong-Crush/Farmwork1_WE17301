7c21a1a46a4dbc1cca40f4c0c52604d7538c20f2
PhanThanhDong-Crush <97138310+PhanThanhDong-Crush@users.noreply.github.com>
2023-05-19 02:11:48 +0700

    FW1-21 product detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/Client/home/home.component';
 import { LayoutClientComponent } from './layout/layout-client/layout-client.component';
 import { LayoutAdminComponent } from './layout/layout-admin/layout-admin.component';
 import { AboutComponent } from './components/Client/about/about.component';
+import { ProductDetailComponent } from './components/Client/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
     path: '', component: LayoutClientComponent, children: [
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductListComponent },
-      {path:'about',component:AboutComponent}
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: 'about', component: AboutComponent }
     ]
   },
   {
@@ -22,8 +24,8 @@ const routes: Routes = [
   }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+@NgModule( {
+  imports: [ RouterModule.forRoot( routes ) ],
+  exports: [ RouterModule ]
+} )
 export class AppRoutingModule { }
