6f3250f19284d551be48c1a960423716d676fa7c
Thanh <olathanh124@gmail.com>
2023-06-01 22:34:01 +0700

    FW1-63

--- src/app/components/Client/product-list/product-list.component.ts
+++ src/app/components/Client/product-list/product-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { ICategory } from 'src/app/interfaces/category';
 import { IProduct } from 'src/app/interfaces/product';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component( {
   selector: 'app-product-list',
@@ -8,5 +11,24 @@ import { IProduct } from 'src/app/interfaces/product';
 } )
 export class ProductListComponent
 {
+  products:IProduct[]=[]
+  categories:ICategory[]=[]
+
+  constructor (
+      private productServices:ProductService,
+      private categoryService:CategoryService
+    ){
+    this.productServices.getAllProduct().subscribe(data =>{
+      this.products =data
+    },err=>console.log(err)
+    )
+
+    this.categoryService.getAllCategory().subscribe(data =>{
+      this.categories =data
+    },err=>console.log(err)
+    )
+  }
+
+
   
 }
