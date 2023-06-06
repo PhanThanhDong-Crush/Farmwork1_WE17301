4bf4716279c6c3dfdbed3639b7577deaed8bdb47
bac <nxb167200@gmail.com>
2023-06-06 23:01:06 +0700

    feat/FW1-49: in ra 3 san pham mơi refs:FW1-49

--- src/app/components/Client/home/home.component.ts
+++ src/app/components/Client/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  item:any = {
+    data:[]
+  }
+  products:IProduct[]=[]
 
+  constructor (private productServices:ProductService){
+    this.productServices.getNewProduct().subscribe((data) =>{
+      this.item=data;
+      
+      
+    },err=>console.log(err)
+    )
+  }
 }
