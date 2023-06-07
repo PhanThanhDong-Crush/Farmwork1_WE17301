f389b9785cb742d2cdcf09d7006896ef8fc2c6cb
bac <nxb167200@gmail.com>
2023-06-07 13:20:00 +0700

    fix: loi ket noi api category - sửa id category interface -sửa kiểu dữ liệu id lấy được trên params từ Number sang String refs:FW1-88

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService
 
   getAllProduct (): Observable<IProduct[]>
   {
-    return this.http.get<IProduct[]>( `http://localhost:3000/products` )
+    return this.http.get<IProduct[]>( `http://localhost:8080/api/products` )
   }
   getNewProduct (): Observable<IProduct[]>
   {
@@ -22,21 +22,21 @@ export class ProductService
 
   getOneProduct ( id: Number | String ): Observable<IProduct>
   {
-    return this.http.get<IProduct>( `http://localhost:3000/products/${ id }` )
+    return this.http.get<IProduct>( `http://localhost:8080/api/products/${ id }` )
   }
 
   addProduct ( product: IProduct ): Observable<IProduct>
   {
-    return this.http.post<IProduct>( `http://localhost:3000/products`, product )
+    return this.http.post<IProduct>( `http://localhost:8080/api/products/add`, product )
   }
 
   editProduct ( product: IProduct ): Observable<IProduct>
   {
-    return this.http.patch<IProduct>( `http://localhost:3000/products/${ product.id }`, product )
+    return this.http.patch<IProduct>( `http://localhost:8080/api/products/edit/${ product._id }`, product )
   }
 
   deleteProduct ( id: Number | String ): Observable<IProduct>
   {
-    return this.http.delete<IProduct>( `http://localhost:3000/products/${ id }` )
+    return this.http.delete<IProduct>( `http://localhost:8080/api/products/${ id }` )
   }
 }
