4a0ac9cfea383dbe10c148f93a5e337402f0f820
PhanThanhDong-Crush <dongptph28020@fpt.edu.vn>
2023-06-07 16:55:22 +0700

    feat: phân trang shop page refs: FW1-85

--- src/app/components/Client/product-list/product-list.component.ts
+++ src/app/components/Client/product-list/product-list.component.ts
@@ -13,14 +13,17 @@ export class ProductListComponent
 {
   products:IProduct[]=[]
   categories:ICategory[]=[]
-
+  page: number = 1;
+  totalPages: number = 1;
   constructor (
       private productServices:ProductService,
       private categoryService:CategoryService
     ){
       //lấy Product 
-    this.productServices.getAllProduct().subscribe(data =>{
-      this.products =data
+    this.productServices.productPagination(this.page).subscribe(data => {
+      const product = data.data
+      this.totalPages = data.totalPages
+      this.products = product
     },err=>console.log(err)
     )
     //Lấy Categories
@@ -30,6 +33,33 @@ export class ProductListComponent
     )
   }
 
+    getRange(): number[] {
+    return Array(this.totalPages).fill(0).map((x, i) => i);
+  }
 
+  setPage(i: number) {
+    this.page = i
+    
+    this.productServices.productPagination(this.page).subscribe(data => {
+      const product = data.data
+      this.products = product
+    },err=>console.log(err)
+    )
+  }
+
+  PreviousNext(a: string) {
+    if (a == "Previous") {
+      this.page = this.page - 1
+      if (this.page < 1) {
+        this.page = 1
+      }
+    } else {
+      this.page = this.page + 1
+      if (this.page > this.totalPages) {
+        this.page = this.totalPages
+      }
+    }
+    this.setPage(this.page)
+  }
   
 }
